16cfb471f2e8b1c8c9c68a253e9bdec276839703
René Füchtenkordt <renef132@gmail.com>
2018-04-15 21:12:33 +0200

    updated

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,7 @@
 const functions = require('firebase-functions')
 const colorArray = require('./dictionarys/colorArray')
+const adjectiveAnimalArray = require('./dictionarys/adjectiveAnimalArray')
+const adjectiveThingArray = require('./dictionarys/adjectiveThingArray')
 const adjectiveArray = require('./dictionarys/adjectiveArray')
 const animalArray = require('./dictionarys/animalArray')
 const verbArray = require('./dictionarys/verbArray')
@@ -19,13 +21,15 @@ exports.randomNumber = functions.https.onRequest((request, response) => {
 
 exports.friendlyHash = functions.https.onRequest((request, response) => {
     response.send(
-        selectRandomArrayItem(colorArray.data)
+        selectRandomArrayItem(adjectiveAnimalArray.data)
         + " " +
+        selectRandomArrayItem(colorArray.data)
+        + "-" +
         selectRandomArrayItem(animalArray.data)
         + " " +
         selectRandomArrayItem(verbArray.data)
         + " a " +
-        selectRandomArrayItem(adjectiveArray.data)
+        selectRandomArrayItem(adjectiveThingArray.data)
         + " " +
         selectRandomArrayItem(nounArray.data)
     )

7084e43da3ff8807b38ba6dc75130e3b171461d7
René Füchtenkordt <renef132@gmail.com>
2018-04-17 13:58:54 +0200

    easier words

---
+++ functions/dictionarys/verbFoodArray.js
@@ -0,0 +1,22 @@
+exports.data = [
+    "eating",
+    "cooking",
+    "preparing",
+    "slicing",
+    "serving",
+    "enjoying",
+    "adoring",
+    "making",
+    "baking",
+    "loving",
+    "admiring",
+    "carrying",
+    "frying",
+    "freezing",
+    "selling",
+    "buying",
+    "chopping",
+    "ordering",
+    "wanting",
+    "munching"
+]
--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,13 @@
 const functions = require('firebase-functions')
-const colorArray = require('./dictionarys/colorArray')
+const namesArray = require('./dictionarys/namesArray')
 const adjectiveAnimalArray = require('./dictionarys/adjectiveAnimalArray')
-const adjectiveThingArray = require('./dictionarys/adjectiveThingArray')
-const adjectiveArray = require('./dictionarys/adjectiveArray')
 const animalArray = require('./dictionarys/animalArray')
-const verbArray = require('./dictionarys/verbArray')
-const nounArray = require('./dictionarys/nounArray')
+const verbFoodArray = require('./dictionarys/verbFoodArray')
+const colorArray = require('./dictionarys/colorArray')
+const foodArray = require('./dictionarys/foodArray')
 
 let selectRandomArrayItem = (array) => {
-    return array[Math.floor(Math.random() * array.length)]
+    return array[Math.floor(Math.random() * array.length)].toLowerCase()
 }
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
@@ -19,18 +18,19 @@ exports.randomNumber = functions.https.onRequest((request, response) => {
     response.send(Math.random().toString())
 })
 
+// Generates 14.940.908.286.000 unique random seed words
 exports.friendlyHash = functions.https.onRequest((request, response) => {
     response.send(
+        selectRandomArrayItem(namesArray.data)
+        + " the " +
         selectRandomArrayItem(adjectiveAnimalArray.data)
         + " " +
-        selectRandomArrayItem(colorArray.data)
-        + "-" +
         selectRandomArrayItem(animalArray.data)
         + " " +
-        selectRandomArrayItem(verbArray.data)
-        + " a " +
-        selectRandomArrayItem(adjectiveThingArray.data)
+        selectRandomArrayItem(verbFoodArray.data)
         + " " +
-        selectRandomArrayItem(nounArray.data)
+        selectRandomArrayItem(colorArray.data)
+        + " " +
+        selectRandomArrayItem(foodArray.data)
     )
 })
